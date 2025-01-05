e256bdcad5274aa71295ab7b2d3bc04b96fbfb01
SemenovKo <ksem2005@gmail.com>
2025-01-05 19:48:04 +0300

    Update task.py

--- Лабор. работа 3/Задание 1/task.py
+++ Лабор. работа 3/Задание 1/task.py
@@ -2,29 +2,27 @@ class Book:
     """ Базовый класс книги. """
 
     def __init__(self, name: str, author: str):
-        self.__name = name
-        self.__author = author
+        self._name = name
+        self._author = author
 
     @property
     def name(self):
-        return self.__name
+        return self._name
 
     @property
     def author(self):
-        return self.__author
+        return self._author
 
     def __str__(self):
-        return f"Книга {self.__name}. Автор {self.__author}"
+        return f"Книга {self.name}. Автор {self.author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r})"
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        super().__init__(name=name, author=author)
-        self.__name = name
-        self.__author = author
+        super().__init__(name, author)
         self._pages = pages
 
     @property
@@ -32,21 +30,20 @@ class PaperBook(Book):
         return self._pages
 
     @pages.setter
-    def pages(self, pages: int) -> None:
-        if not isinstance(pages, int):
+    def pages(self, value) -> None:
+        if not isinstance(value, int):
             raise TypeError
-        if pages <= 0:
+        if value <= 0:
             raise ValueError
+        self._pages = value
 
     def __str__(self):
-        return f"Книга {self.__name}. Автор {self.__author}"
+        return f"Книга: {self.name}. Автор: {self.author}. Страниц: {self.pages}"
 
 
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        super().__init__(name=name, author=author)
-        self.__name = name
-        self.__author = author
+        super().__init__(name, author)
         self._duration = duration
 
     @property
@@ -54,11 +51,12 @@ class AudioBook(Book):
         return self._duration
 
     @duration.setter
-    def duration(self, duration: float) -> None:
-        if not isinstance(duration, float):
+    def duration(self, value) -> None:
+        if not isinstance(value, float):
             raise TypeError
-        if duration <= 0:
+        if value <= 0:
             raise ValueError
+        self._duration = value
 
     def __str__(self):
-        return f"Книга {self.__name}. Автор {self.__author}"
+        return f"Книга: {self.name}. Автор: {self.author}. Продолжительность: {self.duration:.2f} ч"
